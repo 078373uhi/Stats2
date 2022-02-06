b9bfc4ababde7a54dcdeff437d5526efe40993bf
Michelle Paterson <078373@uhi.ac.uk>
2022-02-06 11:38:41 +0000

    Add headers fit logistic regression model on binary data

--- Week2work.R
+++ Week2work.R
@@ -3,6 +3,8 @@ install.packages('car')
 library(sjPlot)
 library(car)
 
+##Ordinary linear regression versus logistic regression
+
 ## generate data
 set.seed(1)
 x <- rnorm(200,5,4)        ## sample x from a normal distribution
@@ -16,6 +18,8 @@ plot_model(m, type='pred', show.data=T, ci.lvl = NA)
 ## plot diagnostics
 plot_model(m, type="diag")
 
+##Fitting a linear model on binary data
+
 ## generate data
 set.seed=1
 x = rnorm(200, 5, 4)              ## sample x from a normal distribution
@@ -26,3 +30,15 @@ y = rbinom(200, 1, prob = prob)   ## sample y from probabilities with binomial e
 ## fit linear model and plot
 m_lm = lm(y ~ x)
 plot_model(m_lm, type='pred', show.data=T, ci.lvl=NA)
+
+library(ggfortify)
+autoplot(m_lm)
+
+## Fitting a logistic regression model on binary data
+
+m_glm <- glm(y ~ x, family = binomial(link = 'logit'))
+plot_model(m_glm, type='pred', show.data=T, terms='x [all]')
+
+##Fitting and interpreting a GLM
+
+
