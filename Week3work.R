3438eddc9d35c6d714e67af6af62dd0b22d7d8a0
Michelle Paterson <078373@uhi.ac.uk>
2022-02-14 20:39:11 +0000

    Don't know what the fuck was going on here

--- Week3work.R
+++ Week3work.R
@@ -54,3 +54,55 @@ m2 = lmer(Reaction ~ Days + (1 + Days| Subject), data=d)
 anova(m_base,m1,m2)
 
 tab_model(m_base, m1, m2)
+
+# Another example with more data
+
+set.seed(1)
+groups = 26
+groupsize = 30
+n = 26*30
+
+level1 = tibble(class_id = rep(letters, each=groupsize),
+                hours_studied = rpois(n, 12),
+                wealth = rnorm(n, rep(1:groups, each=groupsize), 3))
+
+
+level2 = tibble(class_id = letters,
+                teacher_exp = rpois(groups, 1:groups/2))
+level2$B0j = 2 + 0.3*level2$teacher_exp + rnorm(groups, 0, 0.4)
+level2$B1j = 0.1 + rnorm(groups, 0, 0.15)
+
+d = left_join(level1, level2, by='class_id')
+d$exam_grade = d$B0j + d$B1j*d$hours_studied + rnorm(n, 0, 0.8)
+
+# Analyzing the data with normal linear regression
+m0 = lm(exam_grade ~ 1, data=d)
+m1 = lm(exam_grade ~ 1 + hours_studied, data=d)
+m2 = lm(exam_grade ~ 1 + hours_studied + wealth, data=d)
+
+tab_model(m0, m1, m2)
+
+m3 = lm(exam_grade ~ 1 + hours_studied + wealth + class_id, data=d)
+tab_model(m3)
+tab_model(m3, rm.terms = paste('class_id', letters, sep=''))
+
+m3 = lm(exam_grade ~ 1 + hours_studied + wealth + class_id + teacher_exp, data=d)
+m3$coefficients
+
+#Analyzing the data with multilevel regression
+m4 <- lmer(exam_grade ~ 1 + hours_studied + wealth + teacher_exp + (1 + hours_studied | class_id), data=d)
+hist(d$hours_studied)
+
+d = mutate(d, hours_studied = hours_studied - mean(hours_studied),
+           wealth = wealth - mean(wealth))
+m4 = lmer(exam_grade ~ 1 + hours_studied + wealth + teacher_exp + (1 + hours_studied | class_id), data=d)
+
+m0 = lmer(exam_grade ~ 1 + (1 | class_id), data=d)
+m1 = lmer(exam_grade ~ 1 + hours_studied + (1 | class_id), data=d)
+m2 = lmer(exam_grade ~ 1 + hours_studied + wealth + (1 | class_id), data=d)
+m3 = lmer(exam_grade ~ 1 + hours_studied + wealth + teacher_exp + (1 | class_id), data=d)
+m4 = lmer(exam_grade ~ 1 + hours_studied + wealth + teacher_exp + (1 + hours_studied | class_id), data=d)
+
+tab_model(m0, m1, m2, m3, m4, show.ci = F)
+
+plot_model(m4,  type="re")
