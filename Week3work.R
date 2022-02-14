4fba60f99a5a6785f1b45a6243db4244287c5bc6
Michelle Paterson <078373@uhi.ac.uk>
2022-02-14 20:26:28 +0000

    Regular linear model added

--- Week3work.R
+++ Week3work.R
@@ -3,3 +3,22 @@ library(sjPlot)
 library(tidyverse)
 install.packages('glmmTMB')
 library(glmmTMB)
+
+# Multilevel modeling in R: a visual explanation
+d <- tibble(Reaction = c(0,1,7,9,17,16,12,10,29,27,24,22,39,36,33,30,49,47,42,42),
+            Days = c(1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4),
+            Subject = c(1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5))
+d$Subject<-factor(d$Subject)
+
+ggplot(d, aes(x=Days, y=Reaction, colour=Subject))+
+  geom_point(size=4)+scale_color_brewer(palette="Dark2")
+
+# Regular linear model
+m = lm(Reaction ~ Days, data=d)
+tab_model(m)
+
+ggplot(d, aes(x=Days, y=Reaction, colour=Subject))+
+  geom_point(size=3)+
+  geom_smooth(method=lm, na.rm = TRUE, fullrange= TRUE,
+              aes(group=1),colour="black", se=FALSE) +
+  scale_color_brewer(palette = "Dark2")
