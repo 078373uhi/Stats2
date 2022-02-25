4c1a4be3d1cfc6c1124858fd129983939aaa213c
Michelle Paterson <078373@uhi.ac.uk>
2022-02-25 19:37:27 +0000

    Add intro

--- Skills_test1.R
+++ Skills_test1.R
@@ -8,3 +8,8 @@ library(glmmTMB)
 patients <- read.csv("~/Stats2/ST1.csv")
 
 view(patients)
+
+# It is my intention to investigate if any of the patient variables (hospital, 
+# sex, age, duration of stay) are linked to whether the patient survives or dies.
+
+
