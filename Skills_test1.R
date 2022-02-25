6a5a43838b5cf419f50f01deca300cd9079347fc
Michelle Paterson <078373@uhi.ac.uk>
2022-02-25 19:50:47 +0000

    first try

--- Skills_test1.R
+++ Skills_test1.R
@@ -12,4 +12,13 @@ view(patients)
 # It is my intention to investigate if any of the patient variables (hospital, 
 # sex, age, duration of stay) are linked to whether the patient survives or dies.
 
+#convert yes/no to 0/1 in death column
+patients$death<-ifelse(patients$death=="yes",1,0)
 
+#create model
+m_glm = glm(death ~ loc + age + sex + dur, 
+             data=patients, family=binomial)
+
+tab_model(m_glm)
+
+plot_model(m_glm, type='pred', grid = T)
