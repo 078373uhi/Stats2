01159987c1331486bc3826eb8655c8de583c8d9c
Michelle Paterson <078373@uhi.ac.uk>
2022-03-13 11:01:55 +0000

    Trump scrape

--- Skills_test2.R
+++ Skills_test2.R
@@ -11,7 +11,10 @@ library(wordcloud)
 library(igraph)
 library(ggraph)
 
-# The text chosen for this analysis was the first State of the Union address from 
+# •	Choose at least two different US Presidents for your analysis.
+# •	Scrape text from the website and create a corpus.
+
+# The text chosen for this analysis was the first State of the Union Address from 
 # each of the five most recent American presidents; Biden, Trump, Obama, Bush Jr
 # and Clinton.
 
@@ -48,6 +51,34 @@ tail(biden_text, 10)
 
 # SECOND TEXT
 trump <- read_html('https://www.presidency.ucsb.edu/documents/address-before-joint-session-the-congress-the-state-the-union-25')
+
+trump_nodes <- trump %>%
+  html_nodes("p")
+
+# inspect text
+length(trump_nodes)
+
+head(trump_nodes)
+
+# create tibble from text
+trump_text <- tibble(source='bid',text = trump %>%
+                       html_nodes("p") %>%
+                       html_text())
+
+# inspect tibble
+head(trump_text)
+
+tail(trump_text, 10)
+
+# remove unecessary introduction/ending lines and brackets of applause etc.
+trump_text$text <- str_remove(trump_text$text, "\\[[^\\)]+\\]")
+n <- dim(trump_text)[1] 
+trump_text <- trump_text[3:(n-6),] 
+
+# check final text
+head(trump_text)
+
+tail(trump_text, 10)
 
 # THIRD TEXT
 obama <- read_html('https://www.presidency.ucsb.edu/documents/address-before-joint-session-the-congress-the-state-the-union-17')
