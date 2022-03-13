5769eb4da945e22aad68afa0f532b6fb6489010d
Michelle Paterson <078373@uhi.ac.uk>
2022-03-13 09:29:04 +0000

    scrape biden text

--- Skills_test2.R
+++ Skills_test2.R
@@ -11,3 +11,23 @@ library(wordcloud)
 library(igraph)
 library(ggraph)
 
+# get first text off the webpage
+biden <- read_html('https://www.presidency.ucsb.edu/documents/address-before-joint-session-the-congress-the-state-the-union-28')
+
+biden_nodes <- biden %>%
+  html_nodes("p")
+
+#inspect text
+length(biden_nodes)
+
+head(biden_nodes)
+
+#create tibble from text
+biden_text <- tibble(source='bid',text = biden %>%
+                         html_nodes("p") %>%
+                         html_text())
+
+#inspect tibble
+head(biden_text)
+
+tail(biden_text, 10)
