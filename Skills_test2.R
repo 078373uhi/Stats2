624e4d80f28eb1f0bf2f21377cf1eeecaf326318
Michelle Paterson <078373@uhi.ac.uk>
2022-03-13 09:25:57 +0000

    Load packages

---
+++ Skills_test2.R
@@ -0,0 +1,13 @@
+# load required packages
+library(rvest)
+library(tidyverse)
+library(tidytext)
+library(patchwork)
+library(stringr)
+library(ggplot2)
+library(widyr)
+library(wordcloud2) 
+library(wordcloud)
+library(igraph)
+library(ggraph)
+
